3866d2c1792a73f6c4c2615488e79fe6b749f5f6
Saurabh Sharma <saurabhpysharma@gmail.com>
2015-11-10 12:52:52 +0530

    project directory for cookiecutter

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/saurabh/Projects/python/django-projects/django_bootstrap/venv/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
