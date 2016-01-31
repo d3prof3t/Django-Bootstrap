8cc0fb728097e470385f93cf118904fc0f080ccc
Saurabh Sharma <saurabhpysharma@gmail.com>
2016-01-31 14:02:28 +0530

    removed ansible playbooks from the project code

--- deploy/deploy_prod.sh
+++ deploy/deploy_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ./prod/deploy.yml --private-key=../../prod_servers_config/128.199.70.225_prod_key -K -u deployer -i ./prod/hosts
+ansible-playbook ./prod/deploy.yml --private-key=<path-to-your-private-ssh-key> -K -u deployer -i ./prod/hosts
