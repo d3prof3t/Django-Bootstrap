1244926a8a7745cd3f2887ae18ef628f0086cfa1
Saurabh Sharma <saurabhpysharma@gmail.com>
2016-02-11 02:35:06 +0530

    testing

--- config/settings/production.py
+++ config/settings/production.py
@@ -55,7 +55,7 @@ SESSION_COOKIE_HTTPONLY = True
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
 # ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
-ALLOWED_HOSTS = ['128.199.75.205']
+ALLOWED_HOSTS = ['128.199.96.60']
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ("gunicorn", )
